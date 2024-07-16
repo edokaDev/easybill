40df546358fcb29416b07441ea5e98831e5bbffc
edoka <peter.edoka.242464@unn.edu.ng>
2024-07-16 18:55:12 +0100

    feat: api routes

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,9 @@
 <?php
 
+use App\Http\Controllers\TransactionController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +15,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::group(['prefix' => 'users'], function () {
+//     Route::get('', [UserController::class, 'index']);
+//     Route::post('', [UserController::class, 'store']);
+//     Route::get('/{user}', [UserController::class, 'show']);
+//     Route::put('/{user}', [UserController::class, 'update']);
+//     Route::delete('/{user}', [UserController::class, 'destroy']);
+//     Route::get('/{user}/transactions', [UserController::class, 'getTransactions']);
+// });
+
+// Route::group(['prefix' => 'transactions'], function () {
+//     Route::get('', [TransactionController::class, 'index']);
+//     Route::post('', [TransactionController::class, 'store']);
+//     Route::get('/{transaction}', [TransactionController::class, 'show']);
+//     Route::put('/{transaction}', [TransactionController::class, 'update']);
+//     Route::delete('/{transaction}', [TransactionController::class, 'destroy']);
+// });
+Route::apiResource('users', UserController::class);
+Route::get('/users/{user}/transactions', [UserController::class, 'getTransactions']);
+Route::apiResource('transactions', TransactionController::class);
