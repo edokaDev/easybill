7b484fc09c2add7e795befe0e4dfc81d48c50d3a
edoka <peter.edoka.242464@unn.edu.ng>
2024-07-16 16:32:41 +0100

    feat: user/transaction resources

---
+++ app/Http/Resources/TransactionResource.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class TransactionResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this.id,
+            'user_id' => $this.user_id,
+            'description' => $this.description,
+            'amount' => $this.amount,
+            'charges' => $this.fees,
+            'status' => $this.status,
+            'transaction_type' => $this.transaction_type,
+            'created_at' => $this.created_at
+        ];
+    }
+}
